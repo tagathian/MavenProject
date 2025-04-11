724a6209ad5a29d44f5ec6203125ef912b03e556
Agathian T <agathiant@gmail.com>
2025-04-12 00:28:08 +0100

    Jenkin JenkinsfileSample file created

---
+++ jenkins/JenkinsfileSample.groovy
@@ -0,0 +1,25 @@
+Pipeline
+{
+    agent any
+
+    stages {
+        stage('Build')
+            {
+                steps{
+                    echo " This is build stage"
+                }
+            }
+        stage('test')
+        {
+            steps{
+                echo "This is testing phase"
+            }
+        }
+        stage("Deploy")
+        {
+            steps {
+                echo "This is Deploy phase"
+            }
+        }
+    }
+}
